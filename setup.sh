f04134d40b6a716639478907e7989da811a6ec32
Matěj Kocián <matej.kocian@gmail.com>
2016-07-24 11:06:51 +0200

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for a in .bash_profile .bashrc .gitconfig .vimrc .xinitrc .config/openbox/rc.xml .config/openbox/autostart ; do
+    filename=$HOME/$a
+    if [ -e $filename ] ; then
+        if ! [ -L $filename ] ; then
+            backup="${filename}_`date +'%Y-%m-%d_%H-%M-%S'`.backup"
+            echo "$filename already exists, backing up as $backup"
+            mv $filename $backup
+        else
+            # it is a symlink, hopefully to the right location
+            # => we do nothing
+            continue
+        fi
+        echo ln -s $a $filename
+    fi
+done
+
+
+mkdir -p $HOME/bin
+cp bin/* $HOME/bin/

29edc097153f41ea085859943c83c4313afb8a14
Matěj Kocián <matej.kocian@gmail.com>
2017-11-11 13:53:24 +0100

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 for a in .bash_profile .bashrc .gitconfig .vimrc .xinitrc .config/openbox/rc.xml .config/openbox/autostart .pdbrc ; do
-    filename=$HOME/$a
-    if [ -e $filename ] ; then
-        if ! [ -L $filename ] ; then
+    filename="$HOME/$a"
+    if [ -e "$filename" ] ; then
+        if ! cmp "$a" "$filename" >/dev/null 2>&1 ; then
             backup="${filename}_`date +'%Y-%m-%d_%H-%M-%S'`.backup"
-            echo "$filename already exists, backing up as $backup"
-            mv $filename $backup
+            echo "$filename already exists (and differs), backing up as $backup"
+            mv "$filename" "$backup"
         else
-            # it is a symlink, hopefully to the right location
-            # => we do nothing
-            continue
+            rm "$filename"
         fi
-        echo ln -s $a $filename
     fi
+    if [ -L "$filename" ] ; then
+        if ! [ "`pwd -P`/$a" = "`readlink -f $filename`" ] ; then
+            echo -n "$filename is a link to "
+            readlink -f "$filename"
+        fi
+        continue
+    fi
+    ln -s "`pwd -P`/$a" "$filename"
 done
 
 
-mkdir -p $HOME/bin
-cp bin/* $HOME/bin/
+mkdir -p "$HOME/bin"
+cp -i bin/* "$HOME/bin/"
